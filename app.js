2161462623dec1a32d0bfb5c245c04492e6268df
suneetha <suneetha.itham@gmail.com>
2014-02-02 11:51:45 +0530

    added cars list in casrs variable

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var app = angular.module('plunker', ['ui.multiselect']);
 
 app.controller('MainCtrl', function($scope) {
 	$scope.name = 'World';
-	$scope.cars = ['Audi','BMW','Honda','Apple','Orange','Banana','A','B','Ho','Ap','Or','Ba'];
+	$scope.cars = ['Audi','BMW','Honda','Toyota','Swift','Tata','Suzuki','Maruti','Waganore'];
 	//$scope.selectedCar = [];
 });
